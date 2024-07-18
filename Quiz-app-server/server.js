43911cbc2708963a2c98172e899772e6b4bdd915
Brajesh-S <Brajesh-S@users.noreply.github.com>
2024-07-18 11:30:51 +0530

    nitpicks

--- Quiz-app-server/server.js
+++ Quiz-app-server/server.js
@@ -11,7 +11,7 @@ const port = 3000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://localhost:3000","https://quiz-app-au1t.onrender.com","https://quiz-app-client-k7i4.onrender.com"],
+    origin: ["http://localhost:3001", "https://quiz-app-au1t.onrender.com","https://quiz-app-au1t.onrender.com","https://quiz-app-client-k7i4.onrender.com"],
     methods: ["POST", "GET", "OPTIONS", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
